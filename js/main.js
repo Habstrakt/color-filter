899ee5ce13b99627b2f3e483b8ad745dfabbdd6b
kukuruza <iamhab@gmail.com>
2021-08-02 01:19:51 +1000

    При клике выделяет кнопку активности

--- js/main.js
+++ js/main.js
@@ -17,7 +17,19 @@ https://webcademy.ru/files/js2020/solaris/white-pure.png
 */
 
 
+const colorItems = document.querySelectorAll('.colorItem');
 
+
+colorItems.forEach(function(item) {
+  item.addEventListener('click', function(event) {
+    if(!event.target.classList.contains('colorItem--active')) {
+      document.querySelectorAll('.colorItem--active').forEach(function(active) {
+        active.classList.remove('colorItem--active');
+      })
+    }
+    item.classList.add('colorItem--active');
+  })
+})
 
 
 
